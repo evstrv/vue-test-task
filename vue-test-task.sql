fd501498612cfe566f7587fdc2de650cba987410
evstrv <nice.evstratov@ya.ru>
2020-08-03 12:23:22 +0300

    vue-test-task

--- vue-test-task.sql
+++ vue-test-task.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Авг 02 2020 г., 13:38
+-- Время создания: Авг 03 2020 г., 11:23
 -- Версия сервера: 10.4.10-MariaDB
 -- Версия PHP: 7.3.12
 
@@ -33,17 +33,22 @@ CREATE TABLE `data_` (
   `title` varchar(256) NOT NULL,
   `description` varchar(256) NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `type` int(12) NOT NULL
+  `type` int(12) NOT NULL,
+  `price` varchar(256) NOT NULL,
+  `country` varchar(256) NOT NULL,
+  `count` varchar(256) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Дамп данных таблицы `data_`
 --
 
-INSERT INTO `data_` (`id`, `title`, `description`, `creation_date`, `type`) VALUES
-(5, 'title', 'description', '2020-08-02 13:25:06', 1),
-(6, 'title', 'desc', '2020-08-02 13:32:09', 3),
-(7, 'qwe', 'desc', '2020-08-02 13:48:51', 5);
+INSERT INTO `data_` (`id`, `title`, `description`, `creation_date`, `type`, `price`, `country`, `count`) VALUES
+(5, 'title', 'description', '2020-08-02 13:25:06', 1, '', '', ''),
+(6, 'title', 'desc', '2020-08-02 13:32:09', 3, '', '', ''),
+(7, 'qwe', 'desc', '2020-08-02 13:48:51', 5, '', '', ''),
+(8, 'заголовок', 'описание', '2020-08-02 17:06:19', 0, '', '', ''),
+(9, 'заголовок', 'описание', '2020-08-02 17:13:19', 0, 'цена', ' страна', 'количество');
 
 --
 -- Индексы сохранённых таблиц
@@ -63,7 +68,7 @@ ALTER TABLE `data_`
 -- AUTO_INCREMENT для таблицы `data_`
 --
 ALTER TABLE `data_`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
